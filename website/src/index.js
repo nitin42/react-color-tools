5fe296195d0478ac1ff6e92c042f2c7de73e07e4
Nitin <tulswani19@gmail.com>
2018-10-02 22:14:10 +0530

    improve website styles

--- website/src/index.js
+++ website/src/index.js
@@ -1,11 +1,69 @@
 import React from 'react'
 import { render } from 'react-dom'
-import { css } from 'emotion'
+import { css, injectGlobal } from 'emotion'
 import { BasicPicker, SchemePicker, GradientPicker } from 'react-color-tools'
+
+const coolhue = require('coolhue')
 
 const BASIC_PICKER = 'Basic Color Picker'
 const SCHEME_PICKER = 'Color Scheme Picker'
 const GRADIENT_PICKER = 'Gradient Picker'
+
+injectGlobal`
+body {
+  font-family: 'Open Sans', sans-serif;
+  color: #2f2f2f;
+}
+`
+
+// `@media only screen and (max-width: 480px) {
+//   body {
+//     padding: 10px;
+//   }
+
+//   .swatches {
+//     width: 50px;
+//     height: 50px;
+//   }
+
+//   img {
+//     width: 300px;
+//     height: 200px;
+//   }
+
+//   .search-input {
+//     width: 250px;
+//     padding: 10px;
+//     font-size: 12px;
+//   }
+
+//   .heading {
+//     font-size: 30px;
+//   }
+
+//   .link {
+//     font-size: 14px;
+//   }
+
+//   .display-swatches {
+//     display: grid;
+//     grid-template-columns: repeat(4, 1fr);
+//     grid-gap: 10px;
+//     justify-items: center;
+//   }
+
+//   .error-message {
+//     font-size: 12px;
+//   }
+
+//   .hex-codes {
+//     font-size: 12px;
+//   }
+
+//   footer {
+//     font-size: 14px;
+//   }
+// }`
 
 const shadow = css`
   -webkit-box-shadow: 10px 10px 9px -11px rgba(122, 121, 122, 1);
@@ -24,6 +82,9 @@ const Heading = props => (
         `
         : `color: ${props.color};
         `};
+      @media only screen and (max-width: 480px) {
+        font-size: 2.3em;
+      }
     `}
   >
     {props.children}
@@ -33,6 +94,34 @@ const Heading = props => (
 Heading.defaultProps = {
   gradientPicker: false
 }
+
+const Description = props => (
+  <p
+    className={css`
+      font-family: 'Satisfy', cursive;
+      font-size: 1.5em;
+      margin-top: -20px;
+      color: #4f4f4f;
+      @media only screen and (max-width: 480px) {
+        font-size: 0.85em;
+      }
+    `}
+  >
+    A set of tools as React components for working with{' '}
+    <span
+      onClick={props.updateGradient}
+      className={css`
+        background-image: ${props.gradient};
+        -webkit-background-clip: text;
+        -webkit-text-fill-color: transparent;
+        user-select: none;
+        cursor: pointer;
+      `}
+    >
+      colors
+    </span>
+  </p>
+)
 
 const GitHubLink = props => (
   <div style={{ marginTop: 20 }}>
@@ -62,24 +151,24 @@ const Container = props => (
   </div>
 )
 
-const Link = props => (
+const Link = ({ underline, url, children, ...rest }) => (
   <a
     className={css`
       font-size: 1.5em;
       text-decoration: none;
       color: #4f4f4f;
-      ${props.underline ? 'border-bottom: 2px solid #4f4f4f;' : null};
+      border-bottom: 2px solid #4f4f4f;
+      @media only screen and (max-width: 480px) {
+        font-size: 1.2em;
+      }
     `}
-    href={props.url}
+    href={url}
     target="_blank"
+    {...rest}
   >
-    {props.children}
+    {children}
   </a>
 )
-
-Link.defaultProps = {
-  underline: true
-}
 
 const Footer = props => (
   <footer style={{ marginTop: 5, bottom: 0 }}>
@@ -90,7 +179,7 @@ const Footer = props => (
       }}
     >
       Made with ❤️ by{' '}
-      <Link url="https://nitin-tulswani.surge.sh/" underline={false}>
+      <Link url="https://nitin-tulswani.surge.sh/" style={{ fontSize: 18 }}>
         Nitin Tulswani
       </Link>
     </p>
@@ -102,7 +191,8 @@ class App extends React.Component {
     color: 'pink',
     pickers: ['Basic Color Picker', 'Gradient Picker', 'Color Scheme Picker'],
     currentPicker: 'Basic Color Picker',
-    gradient: ''
+    gradient: '',
+    descGrad: coolhue.getGradientStyle(5)
   }
 
   changeFormat = e => this.setState({ currentPicker: e.target.value })
@@ -115,6 +205,14 @@ class App extends React.Component {
         {picker}
       </option>
     ))
+  }
+
+  getRandomValue = (min, max) => Math.random() * (max - min) + min
+
+  updateGradient = e => {
+    const number = Math.floor(this.getRandomValue(1, 60))
+
+    this.setState({ descGrad: coolhue.getGradientStyle(number) })
   }
 
   render() {
@@ -131,6 +229,10 @@ class App extends React.Component {
           >
             React Color Tools
           </Heading>
+          <Description
+            gradient={this.state.descGrad}
+            updateGradient={this.updateGradient}
+          />
           <div className={shadow}>
             {this.state.currentPicker === BASIC_PICKER && (
               <BasicPicker
